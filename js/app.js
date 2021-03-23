bfede0d10a3a59490e0385b40d8e484563cd721b
Bayan Khalil <bayan.khalil92@yahoo.com>
2021-03-23 22:44:40 +0200

    lab 3

--- js/app.js
+++ js/app.js
@@ -1,71 +1,146 @@
 `use strict`
 
-
-let keywordsArray = [];
-function Horns(title, image_url,description,keyword,horns) {
-    this.title = title;
-    this.image_url = image_url;
-    this.description = description;
-    this.keyword = keyword;
-    this.horns = horns;
-    keywordsArray.push(this.keyword);
-
-
-}
-
-Horns.prototype.render=function(){
-    let photoTemplate = $('#photo-template').clone();
-    $('main').append(photoTemplate);
-    photoTemplate.find('h2').text(this.title);
-    photoTemplate.find('img').attr('src', this.image_url);
-    photoTemplate.find('p').text(this.description);
-
-    photoTemplate.removeAttr('id');
-
-}
+$(document).ready(function () {
+    let keywordsArray = [];
+    let objArr = [];
+    function Horns(title, image_url, description, keyword, horns) {
+        this.title = title;
+        this.image_url = image_url;
+        this.description = description;
+        this.keyword = keyword;
+        this.horns = horns;
+        objArr.push(this);
+        keywordsArray.push(this.keyword);
 
 
-Horns.prototype.listKewords=function(){
-   
+
+    }
+
+    Horns.prototype.render = function () {
+        let $photoTemplate = $('#photo-template').html();
+        let html = Mustache.render($photoTemplate, this);
+        console.log(html);
+        $('main').append(html);
+
+
+        // let photoTemplate = $('#photo-template').clone();
+        // photoTemplate.find('h2').text(this.title);
+        // photoTemplate.find('img').attr('src', this.image_url);
+        // photoTemplate.find('p').text(this.description);
+        // photoTemplate.attr('class', `${this.keyword} visible`);
+        // photoTemplate.removeAttr('id');
+        // $('main').append(photoTemplate);
+
+    }
+
+
+    Horns.prototype.listKewords = function () {
+
         let $optionCopy = $('option:first').clone();
-        $optionCopy.attr('value',this.keyword);
+        $optionCopy.attr('value', this.keyword);
         $optionCopy.text(this.keyword);
         $('select').append($optionCopy);
-  
-}
 
-function getHornData() {
 
-    const ajaxSettings = {
-        method: 'get',
-        dataType: 'json'
     }
-    console.log("I will use the ajax ..")
-    $.ajax('data/page-1.json', ajaxSettings).then(data=> {
-        console.log("we got the data!!")
-        console.log(data);
-        console.log(typeof data);
-        data.forEach(element=> {
-            console.log(element);
-            let hornObj = new Horns(element.title, element.image_url,element.description,element.keyword,element.horns);
-            hornObj.render();
-            hornObj.listKewords();
-            $('selecting').append($('<option></option>').attr('value', element.keyword))
-        });
+
+    function getHornData1() {
+
+        const ajaxSettings = {
+            method: 'get',
+            dataType: 'json'
+        }
+        console.log("I will use the ajax ..")
+        $.ajax('data/page-1.json', ajaxSettings).then(data => {
+            console.log("we got the data!!")
+            console.log(data);
+            console.log(typeof data);
+            data.forEach(element => {
+                console.log(element);
+                let hornObj = new Horns(element.title, element.image_url, element.description, element.keyword, element.horns);
+                hornObj.render();
+                hornObj.listKewords();
+            });
+        })
+    }
+    getHornData1();
+
+    function getHornData2() {
+
+        const ajaxSettings = {
+            method: 'get',
+            dataType: 'json'
+        }
+        console.log("I will use the ajax ..")
+        $.ajax('data/page-2.json', ajaxSettings).then(data => {
+            console.log("we got the data!!")
+            console.log(data);
+            console.log(typeof data);
+            data.forEach(element => {
+                console.log(element);
+                let hornObj = new Horns(element.title, element.image_url, element.description, element.keyword, element.horns);
+                hornObj.render();
+                hornObj.listKewords();
+            });
+        })
+    }
+    getHornData2()
+
+    $('#firstPage').on('click', () => {
+        $('section').remove();
+        $('option:not(:first)').remove();
+        objArr = [];
+        getHornData1();
     })
-}
-$('document').ready(getHornData);
+
+    $('#secondPage').on('click', () => {
+        $('section').remove();
+        $('option:not(:first)').remove();
+        objArr = [];
+        getHornData2()
+    })
 
 
-$('select').on('change', function(event){
-    let options = $('select').find(':selected').text()
-    event.preventDefault();
-    $('h2').hide();
-    $('img').hide();
-    $('p').hide();
-    $(options).show();
+    $('select').on('change', () => {
+        $('section').removeClass('visible');
+        let $buttonValue = $('select option:selected').val();
+        console.log($buttonValue);
+
+
+
+        if ($buttonValue === 'default') {
+            $('section').addClass('visible');
+
+        }
+        $(`[class*=${$buttonValue}]`).addClass('visible').show();
+        console.log($(`[class*=${$buttonValue}]`).addClass('visible'));
+
+    })
+
+    $('#byHorns').on('click', () => {
+        $('section').remove();
+        objArr.sort((a, b) => {
+            return b.horns - a.horns;
+        });
+        objArr.forEach(value => {
+            value.render();
+        });
+        $('section').removeClass('visible');
+        let $buttonValue = $('select option:selected').val();
+        if ($buttonValue === 'default') {
+            $('section').addClass('visible');
+        }
+        $(`${$buttonValue}`).addClass('visible');
+    })
    
+
+
+
+
 });
+
+
+
 
 
 
